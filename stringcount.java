65ad4ef3ba0cb5dbbc15d4d5d870c37463ed6a64
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 09:22:18 +0530

    Add files via upload

---
+++ stringcount.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class MyClass
+{
+
+    public static void main(String args[]
+ {
+   
+ String s;
+   
+ char c;
+  
+  int len,i,count=0;
+  
+  Scanner scan=new Scanner(System.in);
+  
+  s=scan.nextLine();
+  
+  len=s.length();
+  
+  for(i=0;i<len;i++)
+  
+  {
+
+      c=s.charAt(i);
+ 
+     if(c!=' ')
+  
+    {
+      
+    count++;
+  
+    }
+   
+ }
+    
+System.out.print(count);
+
+    }
+  
+  
+}
