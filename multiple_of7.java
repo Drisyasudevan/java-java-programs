42f472e6b31bdc470d77d3e58ecccf601897423f
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 23:46:18 +0530

    Add files via upload

---
+++ multiple_of7.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+public static void main (String[] args) throws java.lang.Exception
+
+{
+ int num;
+ 
+Scanner ob=new Scanner(System.in);
+num=ob.nextInt();
+      
+        if (num%7==0)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+}
+}
