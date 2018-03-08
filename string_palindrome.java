9e8f708911d6bb554baa114dbbc6703c83a01fd3
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 22:34:47 +0530

    Add files via upload

---
+++ string_palindrome.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+
+{
+
+
+public static void main (String[] args) throws java.lang.Exception
+
+
+{
+
+ String original, reverse = ""; 
+      
+Scanner in = new Scanner(System.in);
+ 
+     
+      
+original = in.nextLine();
+ 
+      
+int length = original.length();
+ 
+      
+for ( int i = length - 1; i >= 0; i-- )
+         
+reverse = reverse + original.charAt(i);
+ 
+      
+if (original.equals(reverse))
+         
+System.out.println("yes");
+      
+else
+         
+System.out.println("no");
+
+}
+
+
+}
