ecb5be4f638b6b8b9620eb255da1a8d42c48eb66
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 12:37:28 +0530

    Add files via upload

---
+++ count_of_repeated_numbers.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int n,k,i,count=0;
+	
+int a[]=new int[20];
+	
+Scanner s=new Scanner(System.in);
+	
+n=s.nextInt();
+	
+k=s.nextInt();
+	
+for(i=0;i<n;i++)
+	
+{
+	
+a[i]=s.nextInt();	
+
+}
+	
+for(i=0;i<n;i++)
+	
+{
+		
+if(k==a[i])
+		
+{
+			
+count++;
+		
+}
+		
+	
+}
+		
+System.out.print(count);
+	
+	
+}
+
+}
