db84dd2a7776153d05d2e029173aae4ad55d94e8
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 22:43:54 +0530

    Add files via upload

---
+++ composite_number.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+public static void main (String[] args) throws java.lang.Exception
+
+{
+int n,c1=0,i;
+
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+for (i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        c1=c1+1;
+    }
+    if (c!=2)
+    {
+    System.out.print("yes");
+}
+else
+{
+ System.out.print("no");	
+}
+
+}
+
+}
