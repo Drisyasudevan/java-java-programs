0ff5e00b2aab531e2b537f8f15dcf8155b502939
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 23:36:08 +0530

    Add files via upload

---
+++ capitalize_every_word.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+public static void main (String[] args) throws java.lang.Exception
+
+{
+Scanner ob=new Scanner(System.in);
+       
+        String s=ob.nextLine();
+
+        s=" "+s;
+        String cap="";
+        for(int i=0;i<s.length();i++)
+        {
+            char x=s.charAt(i);
+            if(x==' ')
+            {
+                cap=cap+" ";
+                char y=s.charAt(i+1);
+                cap=cap+Character.toUpperCase(y);
+                i++;
+            }
+            else
+            {
+                cap=cap+x;
+            }
+        }
+        System.out.println(cap);
+}
+}
