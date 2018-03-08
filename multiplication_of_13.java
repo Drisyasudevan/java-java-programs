3bea1b5b2c8312b1b19b2954686348d9f7271f79
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 22:56:43 +0530

    Add files via upload

---
+++ multiplication_of_13.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+public static void main (String[] args) throws java.lang.Exception
+
+{
+int n,i;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+        
+          if(n%13==0){
+          System.out.print("yes");
+          }
+          else
+          {
+          	System.out.print("no");
+          }
+}
+
+}
