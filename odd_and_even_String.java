3cd13f7504702fcaaf7124c1b0f80a236e5183cd
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 21:05:48 +0530

    Add files via upload

---
+++ odd_and_even_String.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+
+import java.lang.*;
+
+
+import java.io.*;
+
+
+class Ideone
+
+{
+
+
+
+public static void main (String[] args) throws java.lang.Exception
+
+
+{
+
+String str;
+
+char c;
+
+int len,i;
+
+Scanner s=new Scanner(System.in);
+
+str=s.nextLine();
+
+len=str.length();
+
+for(i=0;i<len;i++)
+
+{
+
+if(i%2==0)
+
+{
+
+c=str.charAt(i);
+ 
+System.out.print(c);	
+
+}
+
+}
+ 
+System.out.print(" ");
+ 
+for(i=0;i<len;i++)
+
+{
+if(i%2!=0)
+
+{
+
+c=str.charAt(i);
+ 
+System.out.print(c);	
+
+}
+
+}
+
+}
+
+
+}
