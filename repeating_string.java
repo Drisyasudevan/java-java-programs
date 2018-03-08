d54ab5c8f1cc84cbf274ee8565e838fd5ac8eaf9
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 10:29:44 +0530

    Add files via upload

---
+++ repeating_string.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class MyClass
+ {
+   
+ public static void main(String args[])
+ {
+    
+String s;
+int i,k;
+Scanner scan=new Scanner(System.in);
+s=scan.nextLine();
+k=scan.nextInt();
+for(i=0;i<k;i++)
+  {
+    System.out.print(s+"\n");
+ }
+  }
+ 
+   
+}
