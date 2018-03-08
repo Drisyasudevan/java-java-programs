ec1f61cc27387ec08483bea172a2effacdc808f3
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 20:03:23 +0530

    Add files via upload

---
+++ ab_mod_c.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+public static void main (String[] args) throws java.lang.Exception
+
+{
+int a,b,c,res;
+
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+b=s.nextInt();
+c=s.nextInt();
+res=(a*b)%c;
+	System.out.print(res);
+
+}
+
+}
