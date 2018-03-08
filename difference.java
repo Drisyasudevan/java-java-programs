fe04a620c9b04cdcd250613ca00b369ac3aad982
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 22:18:49 +0530

    Add files via upload

---
+++ difference.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+
+import java.lang.*;
+
+import java.util.*;
+
+import java.io.*;
+
+
+class Ideone
+
+
+{
+
+
+
+public static void main (String[] args) throws java.lang.Exception
+
+
+{
+
+int a,b,result;
+
+Scanner s=new Scanner(System.in);
+
+a=s.nextInt();
+
+b=s.nextInt();
+
+	
+result=a-b;
+
+
+
+if(result<0)
+
+{
+	
+result=Math.abs(result);
+	
+System.out.print(result);
+
+}
+
+else
+
+{
+		
+System.out.print(result);
+
+}
+
+}
+
+
+}
