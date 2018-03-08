066ab4857fe40170c3dc8e8dea3b8d1fd1903f90
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 10:38:59 +0530

    Add files via upload

---
+++ string_concatenate.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class MyClass
+ {
+   
+ public static void main(String args[])
+ {
+    
+String s,s2,s3;
+int len1,len2;
+Scanner scan=new Scanner(System.in);
+s=scan.nextLine();
+s2=scan.nextLine();
+s3=s+s2;
+System.out.print(s3);
+ } 
+   
+}
