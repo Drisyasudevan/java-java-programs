5c9aaa2c7efb125bbbfd2486703ef2a13e5f23e1
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 12:20:01 +0530

    Add files via upload

---
+++ power_of_two.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int n,temp,flag=0;
+	
+Scanner s=new Scanner(System.in);
+	
+n=s.nextInt();
+
+temp=n;
+	
+ if(n!=0)
+
+ {
+	
+ while(temp!=1)
+	
+ {
+	
+ if(temp%2!=0)
+	 
+{
+
+flag=1;
+	 
+break;
+	 
+}
+	 	
+temp=temp/2;
+
+}
+
+}
+ 
+  if(flag==1)
+
+    {
+    	
+System.out.print("no");
+ 
+   }
+  
+  else
+ 
+   {
+  
+  System.out.print("yes");	
+
+    }
+	
+}
+
+}
