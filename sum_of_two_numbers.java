b7b191ec209e938cc2f384eef560b788cf1f7843
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 12:00:27 +0530

    Add files via upload

---
+++ sum_of_two_numbers.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+	int a,b,sum=0;
+
+	Scanner s=new Scanner(System.in);
+
+	a=s.nextInt();
+
+	b=s.nextInt();
+
+	sum=a+b;
+	
+	System.out.print(sum);
+	}
+
+}
