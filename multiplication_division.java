dfd670ed66502c50dc3e0a0c717024f12dc49f37
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 18:15:04 +0530

    Add files via upload

---
+++ multiplication_division.java
@@ -0,0 +1,36 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int a,b,c,result;
+	
+	
+Scanner s=new Scanner(System.in);
+	
+	
+a=s.nextInt();
+	
+b=s.nextInt();
+	
+c=s.nextInt();
+	
+result=(a*b)/c;
+	
+System.out.print(result);
+	
+}
+
+}
