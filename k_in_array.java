7ae2121157f0e0dac7ab09bccd532fa6c6bef15a
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 12:33:20 +0530

    Add files via upload

---
+++ k_in_array.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+public static void main (String[] args) throws java.lang.Exception
+
+{
+
+int n,k,i,count=0;
+
+int a[]=new int[20];
+
+Scanner s=new Scanner(System.in);
+
+n=s.nextInt();
+
+k=s.nextInt();
+
+for(i=0;i<n;i++)
+
+{
+a[i]=s.nextInt();
+}
+
+for(i=0;i<n;i++)
+
+{
+
+if(k==a[i])
+	
+{
+count=1;
+
+}
+
+}
+
+if(count==1)
+
+{
+System.out.print("yes");
+
+}
+
+else
+
+{
+System.out.print("no");
+
+}
+}
+
+}
