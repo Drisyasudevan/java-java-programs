ebb01d154bdb71de9e08122950b05817bd8f1ce9
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 12:48:13 +0530

    Add files via upload

---
+++ kth_smallest_number.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int n,k,i,j,temp;
+	
+int a[]=new int[20];
+	
+Scanner s=new Scanner(System.in);
+	
+n=s.nextInt();
+	
+k=s.nextInt();
+	
+for(i=0;i<n;i++)
+	
+{
+	
+a[i]=s.nextInt();	
+	
+}
+	
+for(i=0;i<n;i++)
+	
+{
+	
+for(j=i+1;j<n;j++)
+	
+
+{
+		
+if(a[i]>a[j])
+		
+{
+			
+temp=a[i];
+			
+a[i]=a[j];
+			
+a[j]=temp;
+		
+}
+	
+}
+	
+}
+	
+System.out.print(a[k-1]);
+	
+}
+
+}
