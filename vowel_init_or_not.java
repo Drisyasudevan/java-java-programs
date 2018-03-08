7857cc0febfd2843adec3b6bc505e922ddb6abbd
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 22:39:28 +0530

    Add files via upload

---
+++ vowel_init_or_not.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+
+{
+
+
+public static void main (String[] args) throws java.lang.Exception
+
+
+{
+
+ char ch;
+       int count=0;
+       Scanner s=new Scanner(System.in);
+       ch=s.next().charAt(0);
+      
+       if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            count=1;
+        
+if(count==1)
+{
+	System.out.print("yes");
+}
+else
+{
+System.out.print("no");	
+}
+
+}
+
+
+}
