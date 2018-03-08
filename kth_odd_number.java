d75d6d93c381cc19b960bcc4a0b715079e8b867c
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 12:42:27 +0530

    Add files via upload

---
+++ kth_odd_number.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int n,k,i;
+	
+int a[]=new int[20];
+	
+int b[]=new int[20];
+	
+Scanner s=new Scanner(System.in);
+
+n=s.nextInt();
+	
+k=s.nextInt();
+	
+for(i=0;i<n;i++)
+	
+{
+	
+a[i]=s.nextInt();	
+
+}
+
+for(i=0;i<n;i++)
+
+{
+	
+if(a[i]%2!=0)
+	
+{
+		
+b[i]=a[i];
+	
+}
+	
+}
+	
+System.out.print(b[k]);
+	
+}
+
+}
