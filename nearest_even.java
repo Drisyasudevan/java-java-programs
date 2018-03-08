0a34455a8a5a4722edf170d663004a963b9e648f
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 13:48:58 +0530

    Add files via upload

---
+++ nearest_even.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class MyClass 
+{
+   
+ public static void main(String args[]) 
+{
+    
+int n,n2;
+    
+Scanner s=new Scanner(System.in);
+    
+n=s.nextInt();
+    
+if(n%2==0)
+    
+{
+        
+System.out.print(n);
+   
+ }
+    
+else
+    
+{
+        
+n2=n-1;
+        
+System.out.print(n2);
+    
+}
+    
+}
+
+}
