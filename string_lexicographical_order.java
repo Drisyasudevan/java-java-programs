fe530b2a66b6df5a9ed4864a5e9ec019847d761d
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 21:38:54 +0530

    Add files via upload

---
+++ string_lexicographical_order.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+
+import java.lang.*;
+
+
+import java.io.*;
+
+
+class Ideone
+
+
+{
+
+
+public static void main (String[] args) throws java.lang.Exception
+
+
+{
+
+int len,i;
+String str;
+
+char c;
+
+Scanner s=new Scanner(System.in);
+
+str=s.nextLine();
+
+len=str.length();
+
+for(i=len-1;i>=0;i--)
+
+{
+	
+c=str.charAt(i);
+	
+System.out.print(c);
+
+}
+}
+
+
+}
