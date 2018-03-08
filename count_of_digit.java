9cd0c463e9f9b48f8656e0cc6c8845c49217c120
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 12:24:50 +0530

    Add files via upload

---
+++ count_of_digit.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+public static void main (String[] args) throws java.lang.Exception
+
+{
+
+int n,count=0;
+
+Scanner s=new Scanner(System.in);
+
+n=s.nextInt();
+
+while(n!=0)
+
+{
+
+n=n/10;
+
+count++;
+
+}
+
+System.out.print(count);
+
+}
+
+}
