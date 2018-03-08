04b783d0d6e3ccb8e5763a3665838c0b405b4f1a
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 19:44:11 +0530

    Add files via upload

---
+++ celsius_to_kelvin.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int c;
+	
+double k,d=273.15;
+	
+Scanner s=new Scanner(System.in);
+	
+c=s.nextInt();
+	
+k=c+d;
+	
+System.out.print(k);
+	
+}
+
+}
