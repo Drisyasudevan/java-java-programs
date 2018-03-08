b91d3366883d30f1af8555f69eab235eb5194b97
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 21:26:55 +0530

    Add files via upload

---
+++ gcd.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+
+import java.lang.*;
+
+
+import java.io.*;
+
+
+class Ideone
+
+
+{
+
+
+public static void main (String[] args) throws java.lang.Exception
+
+
+{
+
+int a,b,gcd=1;
+
+Scanner s=new Scanner(System.in);
+
+a=s.nextInt();
+
+b=s.nextInt();
+
+for(int i = 1; i <= a && i <= b; ++i)
+        
+{
+           
+            
+if(a % i==0 && b % i==0)
+                
+gcd = i;
+        
+}
+        
+System.out.print(gcd);
+}
+
+
+}
