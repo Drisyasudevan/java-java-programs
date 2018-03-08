017bb0217670ae7c19b78781794bf64f75efb8f6
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 21:32:49 +0530

    Add files via upload

---
+++ lcm.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+
+import java.lang.*;
+
+
+import java.io.*;
+
+
+class Ideone
+
+
+{
+
+
+public static void main (String[] args) throws java.lang.Exception
+
+
+{
+
+int a,b,lcm;
+
+Scanner s=new Scanner(System.in);
+
+a=s.nextInt();
+
+b=s.nextInt();
+ 
+lcm = (a > b) ? a : b;
+
+        
+        
+while(true)
+        
+{
+            
+if( lcm % a == 0 && lcm % b == 0 )
+            
+{
+                
+System.out.print(lcm);
+                
+break;
+            
+}
+            
+++lcm;
+        
+}
+
+}
+
+
+}
