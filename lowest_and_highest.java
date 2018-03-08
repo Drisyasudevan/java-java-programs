318a3ac1b59fc36e3eb9c5e7045fca6a6b9a79ac
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 11:52:46 +0530

    Add files via upload

---
+++ lowest_and_highest.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+
+{
+
+public static void main (String[] args) throws java.lang.Exception
+
+{
+
+int i,n,j,temp;
+
+int a[]=new int[20];
+
+Scanner s=new Scanner(System.in);
+
+n=s.nextInt();
+
+for(i=0;i<n;i++)
+
+{
+
+a[i]=s.nextInt();
+
+}
+
+for(i=0;i<n;i++)
+
+{
+
+for(j=i+1;j<n;j++)
+
+{
+			
+if(a[i]>a[j])
+
+{
+
+temp=a[i];
+	
+a[i]=a[j];
+
+a[j]=temp;
+	
+}
+
+}
+
+}
+
+System.out.print(a[0]+" "+a[n-1]);
+
+}
+
+}
