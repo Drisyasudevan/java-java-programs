71159cf663ae951d5c6c2f753f59c122cfd4e1de
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 19:24:04 +0530

    Add files via upload

---
+++ sum_of_ntherm_in_ap.java
@@ -0,0 +1,33 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int a,d,n,sum;
+	
+Scanner s=new Scanner(System.in);
+	
+a=s.nextInt();
+	
+d=s.nextInt();
+	
+n=s.nextInt();
+	
+
+sum=a+(n-1)*d;
+	
+System.out.print(sum);
+	
+}
+
+}
