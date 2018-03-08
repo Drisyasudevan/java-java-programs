66b2351346c750abc724e50b0782638533129295
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-09 00:18:14 +0530

    Add files via upload

---
+++ product_is =_even_or_not.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+public static void main (String[] args) throws java.lang.Exception
+
+{
+ int n,n2,pro;
+Scanner ob=new Scanner(System.in);
+n2=ob.nextInt();
+n=ob.nextInt();
+pro=n*n2;
+ if(pro%2==0)
+ {
+ 	System.out.print("even");
+ }
+ else
+ {
+ 	System.out.print("odd");
+ }
+}
+}
