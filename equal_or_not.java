b1b791b837be75a69ecccf0a9176d60927292746
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 22:08:36 +0530

    Add files via upload

---
+++ equal_or_not.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+
+import java.lang.*;
+
+
+import java.io.*;
+
+
+class Ideone
+
+
+{
+
+
+public static void main (String[] args) throws java.lang.Exception
+
+
+{
+
+int a,b,result;
+
+Scanner s=new Scanner(System.in);
+
+a=s.nextInt();
+
+b=s.nextInt();
+
+if(a==b)
+
+{
+
+result=a/b;	
+
+System.out.print(result);
+
+}
+
+if(a!=b)
+
+{
+	
+result=a%b;
+	
+System.out.print(result);
+
+}
+
+
+}
+
+
+}
