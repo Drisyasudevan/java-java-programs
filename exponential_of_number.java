958625f5664355a53c1e8b246db8359394573e48
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 18:47:00 +0530

    Add files via upload

---
+++ exponential_of_number.java
@@ -0,0 +1,38 @@
+
+
+import java.util.*;
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int n,k,i,exp=1;
+	
+	
+Scanner s=new Scanner(System.in);
+	
+n=s.nextInt();
+	
+k=s.nextInt();
+	
+for(i=1;i<=k;i++)
+	
+{
+		
+exp=exp*n;
+	
+}
+	
+System.out.print(exp);
+	
+	
+}
+
+}
