3375eb7e35bc70eb6c208b8c1afb836d45d9700d
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 10:44:57 +0530

    Add files via upload

---
+++ n_within_ten.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class MyClass
+ {
+   
+ public static void main(String args[])
+ {
+ int n;
+ Scanner s=new Scanner(System.in);
+ n=s.nextInt();
+ if((n==1)||(n<=10))
+ {
+     System.out.print("yes");
+ }
+ else
+ {
+     System.out.print("no");
+ }
+ } 
+   
+}
