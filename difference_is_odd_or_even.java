6a0163bfdb7ec130e821c09d079e05697fc987cb
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 23:49:27 +0530

    Add files via upload

---
+++ difference_is_odd_or_even.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+public static void main (String[] args) throws java.lang.Exception
+
+{
+ int n1,n2,dif;
+ 
+Scanner ob=new Scanner(System.in);
+n1=ob.nextInt();
+ n2=ob.nextInt();
+ dif=n1-n2;
+        if (dif%2==0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
+}
+}
