dd44014e29e941865531cfba0d9d6028e9026472
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 18:08:52 +0530

    Add files via upload

---
+++ nth_characters.java
@@ -0,0 +1,45 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int n,i,len;
+	
+String str;
+	
+char c;
+	
+Scanner s=new Scanner(System.in);
+	
+str=s.nextLine();
+	
+n=s.nextInt();
+	
+len=str.length();
+	
+for(i=n-1;i<len;i++)
+	
+{
+	
+c=str.charAt(i);
+
+
+System.out.print(c);
+
+}
+	
+}
+
+}
