df704e8b423d298e85ff75c28f93b9a3c2439544
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 20:52:47 +0530

    Add files via upload

---
+++ area_of_farm.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+import java.lang.*;
+
+
+import java.io.*;
+
+
+class Ideone
+
+{
+
+
+public static void main (String[] args) throws java.lang.Exception
+
+
+{
+
+double l,b,area;
+
+
+Scanner s=new Scanner(System.in);
+
+l=s.nextDouble();
+
+b=s.nextDouble();
+
+area=l*b;
+ 
+System.out.print(area);	
+
+
+}
+
+
+}
