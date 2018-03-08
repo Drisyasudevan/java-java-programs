c0dda9a4eec089fbbb929a1c0672eb4e4f04352c
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 11:41:50 +0530

    Add files via upload

---
+++ digits_in_number.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+	int n,count=0,i;
+
+	Scanner s=new Scanner(System.in);
+
+	n=s.nextInt();
+
+	while(n!=0)
+
+	{
+
+		n=n/10;
+
+		count++;
+
+	}
+
+	System.out.print(count);
+
+	}
+
+}
