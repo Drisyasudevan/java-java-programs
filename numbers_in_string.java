80bcf4896533fce1a304f53582975f1ea3ae4631
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 22:29:19 +0530

    Add files via upload

---
+++ numbers_in_string.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+
+import java.lang.*;
+
+
+import java.io.*;
+
+
+class Ideone
+
+
+{
+
+
+public static void main (String[] args) throws java.lang.Exception
+
+
+{
+
+int len,i;
+
+String str;
+
+char c;
+
+Scanner s=new Scanner(System.in);
+
+str=s.nextLine();
+
+len=str.length();
+
+for(i=0;i<len;i++)
+
+{
+
+c=str.charAt(i);
+
+if(c=='0'||c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9')
+
+{
+
+System.out.print(c);	
+
+}
+
+
+}
+
+}
+
+
+}
