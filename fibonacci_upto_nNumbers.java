76b9be3e0af9fbf097a0876838fa47995505d9ea
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 09:56:33 +0530

    Add files via upload

---
+++ fibonacci_upto_nNumbers.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class MyClass 
+{
+  
+  public static void main(String args[])
+ {
+ 
+   int n,n1=0,n2=1,n3=0,i;
+ 
+   Scanner s=new Scanner(System.in);
+  
+  n=s.nextInt();
+ 
+   for(i=0;i<n;i++)
+ 
+   {
+      
+  System.out.print(n3+" ");
+ 
+       n1=n2;
+    
+    n2=n3;
+     
+   n3=n1+n2;
+   
+ }
+   
+ }
+  
+  
+}
+
