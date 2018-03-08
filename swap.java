77f6556a594550444508583208d7d70065591b5f
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 09:42:04 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class MyClass
+ {
+   
+ public static void main(String args[])
+ {
+    
+int a,b,temp;
+ 
+   Scanner s=new Scanner(System.in);
+ 
+   a=s.nextInt();
+
+    b=s.nextInt();
+ 
+   temp=a;
+  
+  a=b;
+   
+ b=temp;
+   
+ System.out.print(a+" "+b);
+    }
+ 
+   
+}
