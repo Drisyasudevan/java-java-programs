667a8bd97bb72ed13427cc1aaa39f7671b81897d
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 18:53:49 +0530

    Add files via upload

---
+++ divisible_by_two.java
@@ -0,0 +1,46 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	{
+	
+int n;
+	
+	
+Scanner s=new Scanner(System.in);
+	
+n=s.nextInt();
+	
+	
+if(n%2==0)
+	
+{
+		
+n=n/2;
+	
+System.out.print(n);
+	
+}
+	
+else
+	
+{
+	
+System.out.print(n);	
+	
+}
+	
+	
+}
+
+}
