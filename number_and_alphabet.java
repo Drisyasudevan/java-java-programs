d5e531a810dc96cdfea05814302db81412dd17b0
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 18:39:19 +0530

    Add files via upload

---
+++ number_and_alphabet.java
@@ -0,0 +1,37 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int a;
+	
+String str;
+	
+char c;
+	
+Scanner s=new Scanner(System.in);
+	
+str="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+a=s.nextInt();
+	
+c=str.charAt(a-1);
+	
+System.out.print(c);
+	
+	
+}
+
+}
