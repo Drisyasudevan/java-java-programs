831ad487535c326848ec9ee39d6d663a9e466e08
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 11:44:57 +0530

    Add files via upload

---
+++ nextNo.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+	int n,n2;
+
+	Scanner s=new Scanner(System.in);
+
+	n=s.nextInt();
+
+	n2=n+1;
+
+	System.out.print(n2);
+	}
+
+}
