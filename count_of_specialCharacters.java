996fdec3714aed693dcc7e2a3a2b5b80c428ac9c
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 10:24:59 +0530

    Add files via upload

---
+++ count_of_specialCharacters.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class MyClass
+ {
+   
+ public static void main(String args[])
+ {
+    
+String s;
+ 
+   char c;
+
+    int len,spl,i,count=0;
+
+    Scanner scan=new Scanner(System.in);
+
+    s=scan.nextLine();
+   
+ len=s.length();
+    
+for(i=0;i<len;i++)
+ 
+   {
+    
+  c=s.charAt(i);
+ 
+       if((c<=65&&c>=90)||(c<=97&&c>=122))  
+  {
+    
+      count++;
+ 
+     }
+   
+ }
+ 
+ spl=len-count;
+   System.out.print(spl);
+    }
+ 
+   
+}
