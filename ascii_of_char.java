e674f2d9b5a277dca66a7366f2f53c85743cc319
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 20:57:41 +0530

    Add files via upload

---
+++ ascii_of_char.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+import java.lang.*;
+
+
+import java.io.*;
+
+
+class Ideone
+
+{
+
+
+
+public static void main (String[] args) throws java.lang.Exception
+
+
+{
+
+char ch;
+
+int val;
+
+Scanner s=new Scanner(System.in);
+
+ch=s.next().charAt(0);
+
+val=(int)ch;
+ 
+System.out.print(val);	
+
+
+}
+
+
+}
