804824616641137058447386842d8c4371c96406
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 12:51:57 +0530

    Add files via upload

---
+++ number_concat.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int n,k;
+	
+Scanner s=new Scanner(System.in);
+	
+n=s.nextInt();
+	
+k=s.nextInt();
+	
+	
+System.out.print(n);
+	
+System.out.print(k);
+	
+}
+
+}
