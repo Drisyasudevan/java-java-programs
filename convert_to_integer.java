0889c3689fc36678d87c5c665c29f790f0c58f5d
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 23:08:55 +0530

    Add files via upload

---
+++ convert_to_integer.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+public static void main (String[] args) throws java.lang.Exception
+
+{
+
+double n;
+
+Scanner s=new Scanner(System.in);
+n=s.nextDouble();
+n=Math.ceil(n);
+    System.out.print(n);
+
+}
+}
