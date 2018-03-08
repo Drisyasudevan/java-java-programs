f7896e22dfa2f76ee15b7e8f19093d7706910ebd
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 21:14:37 +0530

    Add files via upload

---
+++ isogram.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+
+import java.lang.*;
+
+
+import java.io.*;
+
+
+class Ideone
+
+
+{
+
+
+public static void main (String[] args) throws java.lang.Exception
+
+
+{
+
+String str;
+
+
+int len,i;
+
+Scanner s=new Scanner(System.in);
+
+str=s.nextLine();
+
+len=str.length();
+
+str = str.toLowerCase();
+        
+len = str.length();
+         
+        
+char arr[] = str.toCharArray();
+         
+        
+Arrays.sort(arr);
+        
+for(i = 0;i < len-1;i++)
+        
+{
+            
+if(arr[i] == arr[i+1])
+                
+System.out.print("no");
+        
+}
+        
+System.out.print("yes");
+
+}
+
+
+}
