46fcfcea2f697fbbace16323e0a6ec66c3307b4e
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-09 06:41:17 +0530

    Add files via upload

---
+++ sum_of_digits.java
@@ -0,0 +1,39 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int n,t,sum=0;
+	
+Scanner s=new Scanner(System.in);
+	
+n=s.nextInt();
+	
+while(n!=0)
+	
+{
+		
+t=n%10;
+		
+sum=sum+t;
+		
+n=n/10;
+	
+}
+	
+System.out.print(sum);
+	
+}
+
+}
