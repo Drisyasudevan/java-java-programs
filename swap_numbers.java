26bf0142251617406838d2ec4e96718923217840
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-09 06:45:51 +0530

    Add files via upload

---
+++ swap_numbers.java
@@ -0,0 +1,35 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int n,t,n2;
+	
+Scanner s=new Scanner(System.in);
+	
+n=s.nextInt();
+	
+n2=s.nextInt();
+	
+t=n;
+	
+n=n2;
+	
+n2=t;
+	
+System.out.print(n+" "+n2);
+	
+}
+
+}
