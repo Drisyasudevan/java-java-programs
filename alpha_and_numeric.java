f03db34041a90a0e56ea4fd6efe5276a6c673959
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-09 07:48:33 +0530

    Add files via upload

---
+++ alpha_and_numeric.java
@@ -0,0 +1,54 @@
+
+
+import java.util.*;
+
+
+import java.lang.*;
+
+
+import java.io.*;
+
+
+public class Ideone
+{
+  
+public static void main(String[] args)
+  
+{
+    
+String str;
+    
+char c;
+    
+int len,i;
+    
+Scanner scanf = new Scanner(System.in);
+ 
+str=s.nextLine();
+ 
+len=str.length();
+ 
+for(i=0;i<len;i++)
+ {
+ 
+c=str.charAt(i);
+ if((c>=60&&c<=90)||(c>=97&&c<=122))||(c=='0'||c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9'))
+ {
+ 	
+System.out.print("yes");
+ 
+}
+
+ else
+ {
+ 	
+System.out.print("no");
+ 
+}
+
+ }
+
+    
+  }
+
+}
