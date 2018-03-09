1044d58195ec9369b3e55b0431937b6b946f3d80
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-09 07:01:16 +0530

    Add files via upload

---
+++ bitwise_swap.java
@@ -0,0 +1,34 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int n,t,n2;
+	
+Scanner s=new Scanner(System.in);
+	
+n=s.nextInt();
+	
+n2=s.nextInt();
+	
+n=n^n2;
+	
+n2=n^n2;
+	
+n=n^n2;	
+System.out.print(n+" "+n2);
+	
+}
+
+}
