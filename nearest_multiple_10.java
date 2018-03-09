8ad0d081ef7a4cb37929cb5fcd93e817b584af79
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-09 08:19:00 +0530

    Add files via upload

---
+++ nearest_multiple_10.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+ 
+class GFG {
+     
+    static int round(int n)
+    {
+        int a = (n / 10) * 10;
+          
+        int b = a + 10;
+      
+        return (n - a > b - n)? b : a;
+    }
+     
+    
+    public static void main(String[] args) 
+    {
+         int n;
+         Scanner s=new Scanner(System.in);
+         n=s.nextInt();
+         System.out.println(round(n));
+    }
+}
