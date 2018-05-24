a9040e144cee0787d99ff51723eb3833b4becd20
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-05-24 18:30:30 +0530

    Add files via upload

---
+++ repeated_Number.java
@@ -0,0 +1,110 @@
+﻿
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+class Ideone
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+	int n,i,j,k,l=0,temp;
+
+	int a[]=new int[20];
+
+	int b[]=new int[20];
+
+	Scanner s=new Scanner(System.in);
+
+	n=s.nextInt();
+
+	for(i=0;i<n;i++)
+
+	{
+
+		a[i]=s.nextInt();
+
+	}
+
+	for(i=0;i<n;i++)
+
+	{
+
+		if(a[i]!=0){
+
+		k=0;
+
+		for(j=1;j<n;j++)
+
+		{
+			
+		if((a[j]!=0)){
+
+			if(a[i]==a[j])
+
+			{
+
+			k++;
+
+			a[j]=0;
+
+			}
+
+			}
+
+		}
+
+	if(k>0)
+
+	{
+
+		b[l]=a[i];
+
+		l++;
+
+		
+	}
+
+		}
+
+	}
+
+	for(i=0;i<l;i++)
+
+	{
+
+		for(j=1;j<l;j++)
+
+		{
+
+			if(b[i]<b[j])
+
+			{
+
+				temp=b[i];
+
+				b[i]=b[j];
+
+				b[j]=temp;
+
+			}
+
+		}
+
+	}
+
+	for(i=0;i<l;i++)
+
+	{
+
+		System.out.print(b[i]+" ");
+
+	}
+
+	}
+
+}
