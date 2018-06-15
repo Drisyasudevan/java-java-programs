5ab72f16d88c8d0d0f3ad31c90b02ccc5ee50c24
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-06-15 20:15:14 +0530

    Add files via upload

---
+++ nth_LArgest_Number.java
@@ -0,0 +1,63 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+		int n,m,i,j,temp;
+
+		int a[]=new int[20];
+
+		Scanner s=new Scanner(System.in);
+
+		n=s.nextInt();
+
+		m=s.nextInt();
+
+		for(i=0;i<n;i++)
+
+		{
+
+			a[i]=s.nextInt();
+
+		}
+
+		for(i=0;i<n;i++)
+
+		{
+
+			for(j=i;j<n;j++)
+
+			{
+
+				if(a[i]<a[j])
+
+				{
+
+					temp=a[i];
+
+					a[i]=a[j];
+
+					a[j]=temp;
+
+				}
+
+			}
+
+		}
+
+			System.out.print(a[m-1]+" ");
+
+	}
+
+}
