97a5b2c1c1d66a2aaee95160ae41cad247e8c73e
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-06-15 21:27:23 +0530

    Add files via upload

---
+++ pair_sum_Present_or_not.java
@@ -0,0 +1,76 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+		
+int n,m,i,p,flag=0;
+
+		int a[]=new int[20];
+
+	
+	Scanner s=new Scanner(System.in);
+
+		n=s.nextInt();
+
+		m=s.nextInt();
+
+		for(i=0;i<n;i++)
+
+		{
+
+		a[i]=s.nextInt();
+	
+	}
+
+		for(i=0;i<n;i++)
+
+		{
+
+			for(int j=i;j<n;j++{
+
+			p=a[i]+a[j];
+
+			if(m==p)
+
+			{
+
+				flag=1;
+
+			}
+
+			}
+
+		}
+
+		if(flag==1)
+
+		{
+
+			System.out.print("YES");
+
+		}
+
+		else
+
+		{
+
+				System.out.print("NO");
+
+		}
+
+	}
+
+}
