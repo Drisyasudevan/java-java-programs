dc3f0fe244d698392f278a6b4b1edd2a0d5a531c
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-06-15 20:34:35 +0530

    Add files via upload

---
+++ permutation_String.java
@@ -0,0 +1,41 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+		String str;
+
+		int l,i,p=1;
+
+		char ch;
+
+		Scanner s=new Scanner(System.in);
+	
+		str=s.next();
+
+		l=str.length();
+
+		for(i=1;i<=l;i++)
+
+		{
+
+		p=p*i;
+
+		}
+
+		System.out.print(p);
+
+	}
+
+}
