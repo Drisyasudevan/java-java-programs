1bdda3a73bbcef85ed0d35443ff8557dc150ef9c
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-06-15 21:16:04 +0530

    Add files via upload

---
+++ product_of_elements_Except_i.java
@@ -0,0 +1,70 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+		
+int n,i,j,g,k;
+
+		int a[]=new int[20];
+
+		int b[]=new int[20];
+
+		Scanner s=new Scanner(System.in);
+
+		n=s.nextInt();
+
+		for(i=0;i<n;i++)
+
+		{
+
+			a[i]=s.nextInt();
+
+		}
+
+		for(i=0;i<n;i++)
+
+		{
+
+			g=1;
+
+			for(j=0;j<n;j++)
+
+			{
+
+			if(a[i]!=a[j])
+
+			{
+
+			g=g*a[j];
+	
+		}
+
+			}
+
+			b[i]=g;
+
+		}
+
+		for(i=0;i<n;i++)
+
+		{
+
+			System.out.print(b[i]+" ");
+
+		}
+
+	}
+
+}
