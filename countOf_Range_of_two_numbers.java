a80d6466394fd7a3cb7a48c63b48c52b40612254
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-06-15 20:54:42 +0530

    Add files via upload

---
+++ countOf_Range_of_two_numbers.java
@@ -0,0 +1,33 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+			int a,b,c,d;
+
+		Scanner s=new Scanner(System.in);
+
+		a=s.nextInt();
+
+		b=s.nextInt();
+
+		c=b-a;
+
+		d=c-1;
+
+		System.out.print(d);
+
+	}
+
+}
